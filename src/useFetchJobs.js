e1a5a874f84039c1aac07d13fb6976246431ea76
Matt Heslington <matt.heslington@icloud.com>
2020-08-10 23:45:13 +0800

    Expand reducer

--- src/useFetchJobs.js
+++ src/useFetchJobs.js
@@ -1,4 +1,5 @@
 import { useReducer } from 'react'
+import axios from 'axios'
 
 
 
@@ -8,17 +9,26 @@ const ACTIONS = {
     ERROR:'error'
 }
 
+const BASE_URL = 'https://jobs.github.com/positions.json'
+
 function reducer(state, action) {
     switch (action.type) {
         case ACTIONS.MAKE_REQUEST:
-            return{ loading:true, jobs:[] }
+            return { loading:true, jobs:[] }
         case ACTIONS.GET_DATA:
-            return{ ...state, loading: false, jobs: action.payload.jobs }
+            return { ...state, loading: false, jobs: action.payload.jobs }
         case ACTIONS.ERROR:
-            XXXXXXXXXXX
+            return { ...state, loading: false, error: action.payload.error, jobs:[] }
+        default:
+            return state
     }
 }
 
 export default function useFetchJobs(params, page) {
     const [state, dispatch] = useReducer(reducer, { jobs: [], loading: true, })
+
+    useEffect(() => {
+        dispatch({ type: ACTIONS.MAKE_REQUEST})
+        axios.get(BASE_URL)
+    }, [params, page])
 }
